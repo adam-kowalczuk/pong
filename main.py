ec25b04e523721fc331e5a03aaa32240da6b47af
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-12-03 15:16:00 -0800

    Fix ball speed error

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
-    ball.move(ball.move_speed)
+    ball.move()
 
     # Detect collision with wall
 

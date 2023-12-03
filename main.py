aaf0b2cf80086cd915c46b1eb1e8e0498f1d8262
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-12-03 14:06:47 -0800

    Import Paddle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,26 +7,21 @@ screen.setup(width=800, height=600)
 screen.title("Pong!")
 screen.tracer(0)
 
-paddle = Turtle("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+# def go_up():
+#     new_y = paddle.ycor() + 20
+#     paddle.goto(paddle.xcor(), new_y)
 
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+# def go_down():
+#     new_y = paddle.ycor() - 20
+#     paddle.goto(paddle.xcor(), new_y)
 
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+# screen.onkey(go_up, "Up")
+# screen.onkey(go_down, "Down")
 
 game_is_on = True
 while game_is_on:
